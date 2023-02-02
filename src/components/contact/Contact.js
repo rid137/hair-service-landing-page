a1a87753d3fc69eee06aacfdccb668bc2865e42f
roycoMedia <makinderidwan73@gmail.com>
2023-02-02 22:17:27 +0100

    first commit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -55,7 +55,8 @@ const Contact = () => {
 
 
 const backgroundStyle = {
-    backgroundImage: `linear-gradient( rgba(0, 0, 0, .6),rgba(0, 0, 0, .6)),url(${blackBg})`,
+    // backgroundImage: `linear-gradient( rgba(0, 0, 0, .6),rgba(0, 0, 0, .6)),url(${blackBg})`,
+    backgroundImage: `url(${blackBg}`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     // backgroundRepeat: 'noRepeat',
